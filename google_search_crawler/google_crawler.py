68ef8f49ea367c2a8501e71d88c78f5267b8fe38
Shane Yu <shaneyu07@gmail.com>
2018-04-09 23:17:19 +0000

    finished data format convert

--- google_search_crawler/google_crawler.py
+++ google_search_crawler/google_crawler.py
@@ -1,8 +1,9 @@
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.poolmanager import PoolManager
-import ssl, requests, urllib, sys, json, pyprind
+import ssl, requests, urllib, sys, json
 from bs4 import BeautifulSoup
 from pprint import pprint
+import ast
 
 
 class MyAdapter(HTTPAdapter):
@@ -52,10 +53,53 @@ class GoogleSearch(object):
         with open('GoogleSearch/{}.json'.format(self.keyword), 'w', encoding='utf-8') as f:
             json.dump(self.json, f)
 
+class process_crawler_result(object):
+    """docstring for process_crawler_result"""
+    def __init__(self):
+        pass
+
+    def checking(self):
+        """產生json檔"""
+        output = []
+        count = 0
+        with open('./googleresult.json', 'r') as rf:
+            for line in rf:
+                one_line = ast.literal_eval(line)
+                output.append(one_line)
+                count += 1
+                if count % 5000 == 0:
+                    print(count)
+
+        print(len(output))
+        with open('result.json', 'w') as wf:
+            json.dump(output, wf)
+
+    def check_json(self):
+        with open('./result.json', 'r') as rf:
+            content = json.load(rf)
+
+        c = 0
+        previous_label = ''
+        for item in content:
+            if item['label'] == previous_label:
+                continue
+            pprint(item)
+            print('---------------------------------------------')
+            previous_label = item['label']
+            c += 1
+            if c == 500:
+                break
+
+
+
 if __name__ == '__main__':
-    g = GoogleSearch(MyAdapter, '中興大學')
-    g.getProf()
-    pprint(g.json)
+    p_obj = process_crawler_result()
+    p_obj.check_json()
+
+
+    # g = GoogleSearch(MyAdapter, '中興大學')
+    # g.getProf()
+    # pprint(g.json)
 
 
     # import time, random
