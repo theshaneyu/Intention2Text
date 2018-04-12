11a1609024cfca881c2fa49fa876afeac04735c4
Shane Yu <shaneyu07@gmail.com>
2018-04-12 22:17:58 +0000

    preprocessing.py in porgress

--- google_search_crawler/preprocessing_for_crawler_result.py
+++ google_search_crawler/preprocessing_for_crawler_result.py
@@ -1,5 +1,7 @@
 import json
 from pprint import pprint
+from opencc import OpenCC
+import random
 
 
 class process_crawler_result(object):
@@ -24,10 +26,12 @@ class process_crawler_result(object):
             json.dump(output, wf)
 
     def gen_final_json(self, content):
-        """
+        """功能：1) 去掉重複label的項目 2) 簡轉繁 3) 處理成preprocessing可以吃的格式
         輸入：爬蟲結果的json檔
-        回傳：每次搜尋的第一個結果串成的dict
+        回傳：preprocessing可以吃的格式的dict
         """
+        openCC = OpenCC('s2twp')  # 簡轉繁
+
         c = 0
         previous_label = ''
         output_list = []
@@ -35,12 +39,12 @@ class process_crawler_result(object):
             if item['label'] == previous_label:
                 continue
             item_dict = {}
-            item_dict['discription'] = item['label']
-            item_dict['context'] = item['search_result']
+            item_dict['discription'] = openCC.convert(item['label'])
+            item_dict['context'] = openCC.convert(item['search_result'])
             output_list.append(item_dict)
             previous_label = item['label'] # 為了只取第一個，其他一樣的都不要
             c += 1
-            if c % 10000 == 0:
+            if c % 1000 == 0:
                 print(c)
 
         print('共有', len(output_list), '筆資料')
@@ -49,9 +53,14 @@ class process_crawler_result(object):
     def main(self):
         with open('./result.json', 'r') as rf: # result.json是學姊給的文檔整理成的json檔
             content = json.load(rf)
+        
         data = self.gen_final_json(content)
+        
         with open('../yahoo_knowledge_data/crawler_result.json', 'w') as wf:
             json.dump(data, wf)
+
+        pprint(random.sample(data, 100))
+
         
 
 
--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -7,16 +7,15 @@ import random
 
 
 """
-代辦事項：
-- 移除注音符號
-- 
+筆記：
+只保留中文字，除了中文字之外，保留逗號，句號。
+空格、半形逗號換成逗號
+接著處理連續問號的問題
+
 """
 class preprocessing(object):
     """docstring for preprocessing"""
     def __init__(self):
-        pass
-
-    def function(self):
         pass
 
     def remove_date_at_beginning_of_context(self, data):
@@ -25,27 +24,92 @@ class preprocessing(object):
             return data[13:]
         return data
 
-    def main(self):
-        with open('../yahoo_knowledge_data/crawler_result.json') as rf:
-            data = json.load(rf) # 90148筆
-        c = 0
-        for item in data:
-            context = self.go_through_all_process(item['context'])
-            # discription = self.go_through_all_process(item['discription'])
-            c += 1
-            if c == 200:
-                break
+    def remove_and_convert_character(self, string):
+        """功能：1) 只保留中文、逗點、句點、數字 2) 空格、半形逗點轉成逗點 3) 數字轉成#
+                4) 清除連續的逗點、句點和#
+        輸入：字串
+        輸出：處理好的字串
+        """
+        clean_str = ''
+        dirty_str = ''
+        for ch in string:
+            if u'\u4e00' <= ch <= u'\u9fff': # 如果是中文
+                clean_str += ch
+            else: # 不是中文
+                if ch == '，' or ch == '。': # 如果是，或。就保留
+                    clean_str += ch
+                elif ch == ',': # 如果是半形的逗號，幫他轉成全型
+                    clean_str += '，'
+                elif ch == ' ':
+                    clean_str += '，'
+                elif ch.isdigit(): # 如果是數字，轉成#
+                    clean_str += '#'
+                else:
+                    dirty_str += ch
+        clean_str = self._remove_sequencial_char(clean_str)
+        print(clean_str)
+        print('<>')
+        print('<>')
+        print('<>')
+        print(dirty_str)
+        
+
+    def _remove_sequencial_char(self, string):
+        """專門處理掉一連串的字元，例如逗號、句號、井字號"""
+        previous_char = ''
+        result_str = ''
+        for c in string:
+            if c == '，':
+                if previous_char == '，' or previous_char == '。':
+                    continue
+                result_str += c
+                previous_char = c
+            elif c == '#':
+                if previous_char == '#':
+                    continue
+                result_str += c
+                previous_char = c
+            elif c == '。':
+                if previous_char == '。' or previous_char == '，':
+                    continue
+                result_str += c
+                previous_char = c
+            else:
+                result_str += c
+                previous_char = c
+        return result_str
+
+
+
+
+
+
 
     def go_through_all_process(self, data):
         """走過所有清理步驟
         輸入：字串
         輸出：處理好的字串
         """
+        # 砍掉context開頭的日期部分
         data = self.remove_date_at_beginning_of_context(data)
-        data = self.simplified_to_tradition()
+        # 砍掉一些不要的字元
+        data = self.remove_and_convert_character(data)
 
-        print(data)
-        print('-----------------------------------')
+        print()
+        print('---------------------------------------------------------------------------')
+        print()
+
+
+    def main(self):
+        with open('../yahoo_knowledge_data/crawler_result.json') as rf:
+            data = json.load(rf) # 90148筆
+
+        # sample東西出來看
+        data = random.sample(data, 100)
+        
+        for item in data:
+            context = self.go_through_all_process(item['context'])
+            # discription = self.go_through_all_process(item['discription'])
 
 
 if __name__ == '__main__':
