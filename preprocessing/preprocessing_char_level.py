8ca5c2f76dfe83585be15ccd2588d4f4f789fb6f
theshaneyu <shaneyu07@gmail.com>
2018-05-11 14:40:27 +0000

    char_level搬家到252

--- preprocessing/preprocessing_char_level.py
+++ preprocessing/preprocessing_char_level.py
@@ -36,41 +36,67 @@ def description_processes(string):
 def split_characters(string):
     return ' '.join(list(string))
 
+def filter_specific_word(self, data):
+    """專門過濾掉一些含有特定關鍵字的item
+    - description當中有「這」的item
+    - description或context中含有「貸」的item
+    """
+    result_list = []
+    for item in data:
+        if not ('這' in item['description'] or
+                '貸' in item['description'] or
+                '貸' in item['context'] or
+                '補習' in item['description'] or
+                '補習' in item['context'] or
+                '家教' in item['description'] or
+                '家教' in item['context']):
+            result_list.append(item)
+    return result_list
+
 
 
 def main():
-    with open('../yahoo_knowledge_data/corpus/init_data.json') as rf:
-        data = json.load(rf)
+    # with open('../yahoo_knowledge_data/corpus/init_data.json') as rf:
+    #     data = json.load(rf)
 
-    # # sample東西出來看
-    # data = random.sample(data, 100)
-    # pprint(data)
+    # err = 0
+    # out_list = []
+    # for item in tqdm(data):
+    #     out_dict = {}
+    #     description = description_processes(item['description'])
+    #     context = context_processes(item['context'])
+    #     if context and description:
+    #         out_dict['context'] = context
+    #         out_dict['description'] = description
+    #         out_list.append(out_dict)
+    #     else:
+    #         err += 1
 
-    err = 0
-    out_list = []
-    for item in tqdm(data):
-        out_dict = {}
-        description = description_processes(item['description'])
-        context = context_processes(item['context'])
-        if context and description:
-            out_dict['context'] = context
-            out_dict['description'] = description
-            out_list.append(out_dict)
-        else:
-            err += 1
+    # with open('../yahoo_knowledge_data/corpus/ver_6/preprocessed_data.json', 'w') as wf:
+    #     json.dump(out_list, wf)
 
-    with open('../yahoo_knowledge_data/corpus/ver_6/preprocessed_data.json', 'w') as wf:
-        json.dump(out_list, wf)
-
-    print('全部資料總共', len(data), '筆')
-    print('前處理無法處理的數量共', err, '筆')
-    print('乾淨資料總計', (len(data) - err), '筆')
+    # print('全部資料總共', len(data), '筆')
+    # print('前處理無法處理的數量共', err, '筆')
+    # print('乾淨資料總計', (len(data) - err), '筆')
 
     """
     以上做完前處理，為了加速所以先存檔，接著下來用讀檔的比較快，之後也可以串起來一次做完。
     """
 
+    with open('../yahoo_knowledge_data/corpus/ver_6/preprocessed_data.json', 'r') as rf:
+        data = json.load(rf)
     
+    # 過濾掉含有特定字串的item
+    data = preprocessing.filter_specific_word(data)
+    # 刪除重複context的item
+    data = preprocessing.remove_duplicate(data)
+
+    # sample東西出來看
+    data = preprocessing._sample_data_to_see(data, 100)
+    pprint(data)
+
+
+
 
 
 
