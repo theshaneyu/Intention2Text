f60456d7d0184615cafdca96b4eeb2d18e966ea3
theshaneyu <shaneyu07@gmail.com>
2018-10-10 14:58:07 +0000

    finished preprocessing

--- google_search_crawler/preprocessing_quora.py
+++ google_search_crawler/preprocessing_quora.py
@@ -1,41 +1,58 @@
 from pprint import pprint
 from random import shuffle
+from sklearn.model_selection import train_test_split
+from data_convert_example import text_to_binary
 
 
 class preprocessing_quora(object):
-    def __init__(self, data_path):
-        self.rf = open(data_path, 'r')
+    def __init__(self, data_path, train_path, valid_path):
+        self.data_path = data_path
+        self.train_path = train_path
+        self.valid_path = valid_path
 
-    def split_train_valid(self):
-        pass
+    def get_data_list(self):
+        data_list = []
+        with open(self.data_path, 'r') as rf:
+            for line in rf:
+                data_list.append(line.replace('\n', ''))
+        return data_list
 
-    def clean_and_format(self):
-        result_set = set()
-        c = 0
-        dup = 0
-        for line in self.rf:
-            c += 1
-            if line[0] == ' ':
-                if line[1:] not in result_set:
-                    result_set.add(line[1:])
-                else:
-                    dup += 1
-            else:
-                result_set.add(line)
+    def split_train_valid(self, data_list):
+        train, valid = train_test_split(data_list, test_size=0.3, random_state=34)
+        return train, valid
 
-        print(dup)
-        # print(len(result_set))
+    def write_file(self, train, valid):
+        with open(self.train_path, 'w') as wf:
+            for item in train:
+                wf.write(item + '\n')
+        with open(self.valid_path, 'w') as wf:
+            for item in valid:
+                wf.write(item + '\n')
 
+    def convert_to_bin(self):
+        text_to_binary(self.train_path, './train/data')
+        text_to_binary(self.valid_path, './valid/data')
 
     def main(self):
-        self.clean_and_format()
-        # self.split_train_valid()
-        self.rf.close()
+        data_list = self.get_data_list()
+        train, valid = self.split_train_valid(data_list)
+        self.write_file(train, valid)
+        self.convert_to_bin()
+
+# def temp():
+#         with open('./corpus/final_result_clean.txt', 'w') as wf:
+#             with open('./corpus/final_result.txt', 'r') as rf:
+#                 for line in rf:
+#                     if line[0] == ' ':
+#                         wf.write(line[1:])
+#                     else:
+#                         wf.write(line)
+
 
 
 if __name__ == '__main__':
-    pre_obj = preprocessing_quora('./corpus/final_result.txt')
+    pre_obj = preprocessing_quora('./corpus/final_result_clean.txt',
+                                  './corpus/train.txt',
+                                  './corpus/valid.txt')
     pre_obj.main()
-        
-
-# 有1143筆重複
+    
